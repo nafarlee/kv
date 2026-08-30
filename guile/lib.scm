f1c253f53064c64f976540b0c0b832e04bd407b5
Nicholas Farley <nicholasafarley@gmail.com>
2026-08-30 19:22:16 -0400

    guile: Create start-blocking-server fn

--- guile/lib.scm
+++ guile/lib.scm
@@ -6,6 +6,13 @@
 (define (parse command)
   "Hello, World!")
 
+(define (start-blocking-server sock handler)
+  (listen sock 128)
+  (let loop ()
+    (let ((client (car (accept sock))))
+      (handler client)
+      (loop))))
+
 (define (create-tcp-socket port)
   (let ((sock (socket PF_INET SOCK_STREAM 0)))
     (setsockopt sock SOL_SOCKET SO_REUSEADDR 1)
